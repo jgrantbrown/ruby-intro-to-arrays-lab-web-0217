e888df42d09fddde126a4691a55836de9a466da6
jason brown <j.grant.brown@gmail.com>
2017-01-04 01:50:19 +0000

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,30 +7,30 @@ end
 
 
 def first_element(array)
-  puts array(1)
+  return array[0]
 end
 
 def array_with_two_elements
   array=[1,2]
-puts array
+return array
 end
 
 def third_element(array)
-  puts array(2)
+  return array[2]
 end
 
 def last_element(array)
-  puts array(-1)
+  return array[-1]
 end
 
 def first_element_with_array_methods(array)
-  puts array.first
+  return array.first
 end
 
 def last_element_with_array_methods(array)
-  puts array.last
+  return array.last
 end
 
-def length_of_array_methods(array)
-  puts array.length
+def length_of_array(array)
+  return array.length
 end

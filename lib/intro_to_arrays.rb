1f4b47c68dbea0c0a44755e1635e82cf89f4517d
jason brown <j.grant.brown@gmail.com>
2017-01-04 01:42:46 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,36 @@
+def instantiate_new_array
+  array=Array.new
+  return array
+end
+
+
+
+
+def first_element(array)
+  puts array(1)
+end
+
+def array_with_two_elements
+  array=[1,2]
+puts array
+end
+
+def third_element(array)
+  puts array(2)
+end
+
+def last_element(array)
+  puts array(-1)
+end
+
+def first_element_with_array_methods(array)
+  puts array.first
+end
+
+def last_element_with_array_methods(array)
+  puts array.last
+end
+
+def length_of_array_methods(array)
+  puts array.length
+end
